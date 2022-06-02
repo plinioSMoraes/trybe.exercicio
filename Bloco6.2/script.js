0f742dea89f33c957a2cb0e3a85a694f1c202016
PlinioMoraes <psmoraes@inf.ufpel.edu.br>
2022-06-02 11:39:55 -0500

    Iniciando exercicio bloco 6.2

---
+++ Bloco6.2/script.js
@@ -0,0 +1,23 @@
+const eraseButton = document.querySelector('.eraseButton');
+const sendButton = document.querySelectorAll('.sendButton');
+
+eraseButton.addEventListener('click', function (originEvent) {
+    let name = document.querySelector('.name');
+    let email = document.querySelector('.email');
+    let reason = document.querySelector('#reason');
+    let destination = document.querySelector('.destination');
+    let data = document.querySelector('.data');
+    let shareImg = document.querySelector('#shareImg');
+    let receiveInfo = document.querySelector('#receiveInfo');
+
+    originEvent.preventDefault();
+
+    email.value = '';
+    name.value = '';
+    reason.value = '';
+    destination.click(); // seleciona a primeira opçao
+    destination.checked = false; // e limpa a primeira opçao
+    data.value = '';
+    shareImg.checked = false;
+    receiveInfo.checked = false;
+})
